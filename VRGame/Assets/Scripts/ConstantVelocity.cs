784dbc3836faebbf55b688d5b533d2178cc3cd41
Melesar <sergey.pikhota@gmail.com>
2018-04-10 13:23:28 +0200

    Moving object

---
+++ VRGame/Assets/Scripts/ConstantVelocity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantVelocity : MonoBehaviour
+{
+    public Vector3 velocity;
+
+    private Rigidbody rb;
+
+    private void Start ()
+    {
+        rb.velocity = velocity;
+    }
+
+    private void Awake ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+}
