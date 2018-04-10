88921c80bb04317f888c10b9b6a9861bd612622d
Melesar <sergey.pikhota@gmail.com>
2018-04-10 14:04:50 +0200

    working movement and collisions

--- VRGame/Assets/Scripts/ConstantVelocity.cs
+++ VRGame/Assets/Scripts/ConstantVelocity.cs
@@ -6,15 +6,11 @@ public class ConstantVelocity : MonoBehaviour
 {
     public Vector3 velocity;
 
-    private Rigidbody rb;
 
-    private void Start ()
+    void Update()
     {
-        rb.velocity = velocity;
+        transform.position += velocity * Time.deltaTime;
     }
 
-    private void Awake ()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+
 }
