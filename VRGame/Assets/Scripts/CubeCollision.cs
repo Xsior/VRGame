6787f6b99349c89be4b5c0b4f545dfa0e88137d7
Melesar <sergey.pikhota@gmail.com>
2018-04-11 09:21:43 +0200

    Cube collision script

---
+++ VRGame/Assets/Scripts/CubeCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CubeCollision : MonoBehaviour
+{
+    public float destroyTimeout = 1f;
+
+    private Collider col;
+
+    private void OnCollisionEnter (Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Hand")) {
+            return;
+        }
+
+        col.enabled = false;
+        Destroy(gameObject, destroyTimeout);
+    }
+
+    private void Awake ()
+    {
+        col = GetComponent<Collider>();
+    }
+}
