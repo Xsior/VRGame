e5a728a235665e4e65621338d86a33a042bc53ab
Melesar <sergey.pikhota@gmail.com>
2018-04-13 18:01:48 +0200

    Changes

---
+++ VRGame/Assets/Scripts/BlocksSegment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocksSegment : MonoBehaviour
+{
+
+    //public Vector3 speed = new Vector3(0, 0, -6);
+    public float length = 0;
+    private List<ConstantVelocity> blocksSpeed;
+
+
+    public void setSpeed(Vector3 speed)
+    {
+        foreach(ConstantVelocity c in blocksSpeed)
+        {
+            c.velocity = speed; 
+        }
+    }
+
+    void Awake()
+    {
+        Debug.Log(transform.childCount);
+        for (int i = 0; i < transform.childCount; i++)
+            blocksSpeed.Add(transform.GetChild(i).gameObject.GetComponent<ConstantVelocity>());
+    }
+
+}
