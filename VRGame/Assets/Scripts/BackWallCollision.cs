8605c42ac567e7fd0836dc66e3acf7b80202de08
Melesar <sergey.pikhota@gmail.com>
2018-04-13 17:26:41 +0200

    Back wall collision

---
+++ VRGame/Assets/Scripts/BackWallCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackWallCollision : MonoBehaviour
+{
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.GetComponent<CubeCollision>() == null) {
+            return;
+        }
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
