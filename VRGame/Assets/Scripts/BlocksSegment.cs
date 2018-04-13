e38ddb3350c5524f3d75d32ecc9c09ec93002b81
Melesar <sergey.pikhota@gmail.com>
2018-04-13 18:46:24 +0200

    Generating hazards by segments

--- VRGame/Assets/Scripts/BlocksSegment.cs
+++ VRGame/Assets/Scripts/BlocksSegment.cs
@@ -7,8 +7,26 @@ public class BlocksSegment : MonoBehaviour
 
     //public Vector3 speed = new Vector3(0, 0, -6);
     public float length = 0;
-    private List<ConstantVelocity> blocksSpeed;
+    private List<ConstantVelocity> blocksSpeed = new List<ConstantVelocity>();
 
+    public float getLength()
+    {
+        if (blocksSpeed.Count <= 0)
+            return 0;
+        
+        Vector3 minPos = blocksSpeed[0].transform.localPosition;
+        Vector3 maxPos = blocksSpeed[0].transform.localPosition;
+
+        foreach(ConstantVelocity c in blocksSpeed)
+        {
+            if (c.transform.localPosition.z < minPos.z)
+                minPos = c.transform.localPosition;
+            if (c.transform.localPosition.z > maxPos.z)
+                maxPos = c.transform.localPosition;
+        }
+        float distance = maxPos.z - minPos.z;
+        return distance;
+    }
 
     public void setSpeed(Vector3 speed)
     {
@@ -20,7 +38,6 @@ public class BlocksSegment : MonoBehaviour
 
     void Awake()
     {
-        Debug.Log(transform.childCount);
         for (int i = 0; i < transform.childCount; i++)
             blocksSpeed.Add(transform.GetChild(i).gameObject.GetComponent<ConstantVelocity>());
     }
--- VRGame/Assets/Scripts/HazardGenerator.cs
+++ VRGame/Assets/Scripts/HazardGenerator.cs
@@ -8,42 +8,65 @@ public class HazardGenerator : MonoBehaviour
     public List<GameObject> blocks;
     public bool started = true;
     public float TimeToNext = 3.5f;
+    public float startSpeed = 5;
+    public float distanceBetweenSegmentsd = 5;
 
+    private float currentSpeed = 5;
     private float timer = 2;
     private float toNextWall = 3;
 
-    private void generateBlock ()
+    private void generateBlock()
     {
         int r = Random.Range(0, blocks.Count);
-        float rX = Random.Range(-1.0f, 1.0f);
-        float rY = Random.Range(0.7f, 1.5f);
         GameObject g = Instantiate(blocks[r], Vector3.zero, transform.rotation, transform);
-        g.transform.localPosition = new Vector3(rX, rY, 35);
+        BlocksSegment b = g.GetComponent<BlocksSegment>();
+        g.transform.localPosition = new Vector3(0, 1, 35);
+        b.setSpeed(new Vector3(0, 0, -currentSpeed));
+        float length;
+        if (b.length == 0)
+        {
+            length = b.getLength();
+        }
+        else
+        {
+            length = b.length;
+        }
+        Debug.Log(length);
+        timer = (length + distanceBetweenSegmentsd) / currentSpeed;
     }
-    private void generatWall ()
+    private void generatWall()
     {
         int r = Random.Range(0, walls.Count);
         GameObject g = Instantiate(walls[r], Vector3.zero, transform.rotation, transform);
         g.transform.localPosition = new Vector3(0, 0, 35);
+        
+    }
+
+    void Start()
+    {
+        currentSpeed = startSpeed;
     }
 
     // Update is called once per frame
-    void Update ()
+    void FixedUpdate()
     {
-        if (started) {
-            if (timer > 0) {
-                timer -= Time.deltaTime;
-            } else {
-                Debug.Log("CHuj");
+        if (started)
+        {
+            if (timer > 0)
+            {
+                timer -= Time.fixedDeltaTime;
+            }
+            else {
                 /*if (toNextWall == 0) {
                     //generatWall();
                     generateBlock();
                     toNextWall = 3;
                 } else {*/
-                    generateBlock();
-                    toNextWall--;
-               // }
-                timer = TimeToNext;
+                generateBlock();
+                toNextWall--;
+                // }
+                //timer = TimeToNext;
+                currentSpeed += 0.05f;
                 if (TimeToNext > 1.5f)
                     TimeToNext -= 0.02f;
             }
