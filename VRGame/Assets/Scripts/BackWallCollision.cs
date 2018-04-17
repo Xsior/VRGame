4d85a571f02bc11aebf798ebfa6c1ae776c5abb9
Jakub Florczyk <jakub.florczyk@ericsson.com>
2018-04-17 13:01:36 +0200

    Added random blocks and glass wall

--- VRGame/Assets/Scripts/BackWallCollision.cs
+++ VRGame/Assets/Scripts/BackWallCollision.cs
@@ -1,16 +1,39 @@
 ﻿using Framework.Events;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BackWallCollision : MonoBehaviour
 {
     public GameEvent onPlayerFailed;
-
-    private void OnTriggerEnter (Collider other)
+    private int hp = 3;
+    private List<Rigidbody> children = new List<Rigidbody>();
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<CubeCollision>() == null) {
+        if (other.GetComponent<CubeCollision>() == null)
+        {
             return;
         }
+        hp--;
+        if (hp <= 0)
+        {
+            BrakeWall();
+            onPlayerFailed.Raise();
+        }
+    }
 
-        onPlayerFailed.Raise();
+    void Awake()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+            children.Add(transform.GetChild(i).gameObject.GetComponent<Rigidbody>());
+    }
+
+
+    void BrakeWall()
+    {
+        foreach (Rigidbody c in children)
+        {
+            c.isKinematic = true;
+            c.AddForce(new Vector3(0, 0, 50));
+        }
     }
 }
