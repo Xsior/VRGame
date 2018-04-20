ae1c865b0c14ca125b81c9dd5680fb5301d23bdb
Melesar <sergey.pikhota@gmail.com>
2018-04-20 17:14:41 +0200

    Spheres to segments

--- VRGame/Assets/Scripts/BlocksSegment.cs
+++ VRGame/Assets/Scripts/BlocksSegment.cs
@@ -7,18 +7,20 @@ public class BlocksSegment : MonoBehaviour
 
     //public Vector3 speed = new Vector3(0, 0, -6);
     public float length = 0;
+
+    public ConstantVelocity itemPrefab;
+
     private List<ConstantVelocity> blocksSpeed = new List<ConstantVelocity>();
 
-    public float getLength()
+    public float getLength ()
     {
         if (blocksSpeed.Count <= 0)
             return 0;
-        
+
         Vector3 minPos = blocksSpeed[0].transform.localPosition;
         Vector3 maxPos = blocksSpeed[0].transform.localPosition;
 
-        foreach(ConstantVelocity c in blocksSpeed)
-        {
+        foreach (ConstantVelocity c in blocksSpeed) {
             if (c.transform.localPosition.z < minPos.z)
                 minPos = c.transform.localPosition;
             if (c.transform.localPosition.z > maxPos.z)
@@ -28,18 +30,25 @@ public class BlocksSegment : MonoBehaviour
         return distance;
     }
 
-    public void setSpeed(Vector3 speed)
+    public void setSpeed (Vector3 speed)
     {
-        foreach(ConstantVelocity c in blocksSpeed)
-        {
-            c.velocity = speed; 
+        foreach (ConstantVelocity c in blocksSpeed) {
+            c.velocity = speed;
         }
     }
 
-    void Awake()
+    private void SpawnCubes ()
     {
-        for (int i = 0; i < transform.childCount; i++)
-            blocksSpeed.Add(transform.GetChild(i).gameObject.GetComponent<ConstantVelocity>());
+        foreach (Transform child in transform) {
+            var itemInstance = Instantiate(itemPrefab, child, false);
+            itemInstance.transform.localPosition = Vector3.zero;
+
+            blocksSpeed.Add(itemInstance);
+        }
     }
 
+    private void Start ()
+    {
+        SpawnCubes();
+    }
 }
