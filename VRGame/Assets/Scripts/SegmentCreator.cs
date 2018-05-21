24914ad36d5af2f1a0e0d56af197ec084f81708b
Jakub Florczyk <jakub.florczyk@ericsson.com>
2018-05-21 23:14:12 +0200

    Begginings of runtime creation of segments

---
+++ VRGame/Assets/Scripts/SegmentCreator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegmentCreator : MonoBehaviour {
+
+    public GameObject parentGO;
+    public GameObject Representation;
+
+    private SteamVR_TrackedObject trackedObj;
+
+    private SteamVR_Controller.Device Controller => SteamVR_Controller.Input((int)trackedObj.index);
+
+    private void Update()
+    {
+        if (Controller.GetHairTriggerDown())
+        {
+            CreatePinapple();
+        }
+    }
+
+
+    private void CreatePinapple()
+    {
+        var child = new GameObject();
+        child.transform.position = transform.position;
+        child.transform.parent = parentGO.transform;
+
+        Instantiate(Representation,transform.position,Quaternion.identity,transform.root);
+
+    }
+
+    private void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+
+
+}
