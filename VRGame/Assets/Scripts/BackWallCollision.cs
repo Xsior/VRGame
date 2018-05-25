89871efa8f0e6904ef0fdd25b5252026fa36e7cb
Jakub Florczyk <jakub.florczyk@ericsson.com>
2018-05-25 17:46:33 +0200

    Breaking of pizza

--- VRGame/Assets/Scripts/BackWallCollision.cs
+++ VRGame/Assets/Scripts/BackWallCollision.cs
@@ -5,9 +5,9 @@ using System.Collections.Generic;
 public class BackWallCollision : MonoBehaviour
 {
     public GameEvent onPlayerFailed;
-    private int hp = 3;
+    public int hp = 9;
     private List<Rigidbody> children = new List<Rigidbody>();
-
+    private int slicesPerHP;
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<CubeCollision>() == null)
@@ -15,9 +15,9 @@ public class BackWallCollision : MonoBehaviour
             return;
         }
         hp--;
+        BrakeWall();
         if (hp <= 0)
         {
-            //BrakeWall();
             onPlayerFailed.Raise();
         }
     }
@@ -26,17 +26,33 @@ public class BackWallCollision : MonoBehaviour
     {
         for (int i = 0; i < transform.childCount; i++)
             children.Add(transform.GetChild(i).gameObject.GetComponent<Rigidbody>());
+        slicesPerHP = Mathf.FloorToInt( children.Count / hp);
+
     }
 
 
     void BrakeWall()
     {
+        if (children.Count == 0)
+            return;
+        if (children.Count < slicesPerHP)
+        {
+            slicesPerHP = children.Count;
+        }
+        for(int i =0; i < slicesPerHP; i++)
+        {
+            children[i].isKinematic = false;
+            children[i].AddForce(new Vector3(-800,100, 0));
+            Destroy(children[i], 3f);
+            children.RemoveAt(i);
+            
+        }
         foreach (Rigidbody c in children)
         {
-            c.isKinematic = false;
-            c.gameObject.GetComponent<Collider>().isTrigger = false;
-            //c.AddForce(new Vector3(0, 0, 50));
-            c.AddExplosionForce(3000, new Vector3(13, 0.2f, 0), 0);
+            //c.isKinematic = false;
+            //c.gameObject.GetComponent<Collider>().isTrigger = false;
+            //c.AddForce(new Vector3(0, 5, 50));
+            //c.AddExplosionForce(3000, new Vector3(13, 0.2f, 0), 0);
         }
     }
 }
