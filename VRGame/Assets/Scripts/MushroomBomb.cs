fd634073e4831fddbff4e8c58e1d0331740c7082
Melesar <sergey.pikhota@gmail.com>
2018-06-08 16:32:35 +0200

    Mushroom bomb

---
+++ VRGame/Assets/Scripts/MushroomBomb.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Framework.Events;
+using UnityEngine;
+
+public class MushroomBomb : MonoBehaviour
+{
+	public string collisionTag;
+	public GameEvent playerLooseEvent;
+
+	public FixedJoint joint;
+
+	private Rigidbody jointRb;
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (!other.gameObject.CompareTag(collisionTag)) {
+			return;
+		}
+
+
+		jointRb.useGravity = true;
+		joint.connectedBody.useGravity = true;
+		jointRb.GetComponent<Rigidbody>().AddForce(Vector3.forward * joint.breakForce);
+		
+		playerLooseEvent.Raise();
+	}
+
+	private void Awake()
+	{
+		jointRb = joint.GetComponent<Rigidbody>();
+	}
+}
