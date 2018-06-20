e66ea667d424521b5cdcd76cfe82a802952fe992
Jakub Florczyk <jakub.florczyk@ericsson.com>
2018-06-20 09:48:18 +0200

    Main Menu kinda

---
+++ VRGame/Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour, IHitListener
+{
+    public String SceneName;
+
+    public void OnHit(Collision collision)
+    {
+        SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+    }
+
+}
