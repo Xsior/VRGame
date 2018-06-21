843ca6893f02fea8c466914bb0c5394af90c3e52
Your Name <you@example.com>
2018-06-21 14:20:38 +0200

    pinapple, watermelon audio effect

---
+++ VRGame/Assets/Scripts/Audio Effects/AudioEffectsManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEffectsManager : MonoBehaviour, IHitListener
+{
+    public AudioClip crashSoft;
+    public AudioClip crashHard;
+
+
+    private AudioSource source;
+    private float lowPitchRange = .75F;
+    private float highPitchRange = 1.5F;
+    private float velToVol = .2F;
+    private float velocityClipSplit = 10F;
+
+
+
+
+
+    public void OnHit(Collision collision)
+    {
+        source.pitch = Random.Range(lowPitchRange, highPitchRange);
+        float hitVol = collision.relativeVelocity.magnitude * velToVol;
+        if (collision.relativeVelocity.magnitude < velocityClipSplit)
+            source.PlayOneShot(crashSoft, hitVol);
+        
+        else 
+            source.PlayOneShot(crashHard, hitVol);
+    }
+}
